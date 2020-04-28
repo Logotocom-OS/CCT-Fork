8b65d7eceffd4bdf6b9f483a30184fb086f1fe49
Logotocom-OS <64005430+Logotocom-OS@users.noreply.github.com>
2020-04-28 08:51:26 -0500

    Update reboot.lua

--- src/main/resources/assets/computercraft/lua/rom/programs/reboot.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/reboot.lua
@@ -1,8 +1,31 @@
+term.clear()
 if term.isColour() then
     term.setTextColour(colours.yellow)
 end
-print("Goodbye")
-term.setTextColour(colours.white)
+print("Starting Reboot... ")
+sleep(0.3)
+print( "Ternimating all running programs..." )
+sleep( 0.05 )
+write( "Done!" )
+sleep( 0.06 )
+print( "Finializing Reboot... " )
+sleep( 0.12 )
+write( "Done!" )
+sleep( 1 )
+print( "Shutting Down..." )
+
+term.setTextColour(colours. green)
 
 sleep(1)
+term.clear()
+
+sleep(2)
+print( "Online, now starting reboot system..." )
+sleep( 1 )
+print( "Finishing..." )
+sleep( 0.03 )
+term.clear()
+
+sleep(1)
+
 os.reboot()
